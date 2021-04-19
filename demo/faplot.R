61d59d88a7737add505356fc81ec88e9941e5b43
Shawn Gilroy <shawnpgilroy@gmail.com>
2021-04-19 13:30:48 -0500

    Update faplot.R

--- demo/faplot.R
+++ demo/faplot.R
@@ -22,35 +22,35 @@ scr_plot(data, aesthetics = list(x = Session,
                                  p = Condition),
         mai = c(0.5, 0.5, 0.1, 0.5),
         omi = c(0.25, 0.25, 0.25, 0.25)) %>%
-  scr_yoverride(c(0, 2)) %>%                                # manually override y-axis and tick interval (tick every 10 units)
+  scr_yoverride(c(0, 2)) %>%                                # manually override y-axis 
   scr_lines(size = 1) %>%                                   # plot lines, using x/y from aesthetics
   scr_points(cex = 2,                                       # plot points, using x/y from aesthetics
              pch = list(                                    # override point marker types (match FA conventions)
-               "Toy Play" = 16,
+               "Toy Play"  = 16,
                "Attention" = 22,
-               "Demand" = 24,
-               "Tangible" = 8
+               "Demand"    = 24,
+               "Tangible"  = 8
              ),
              fill = list(                                   # override point marker colors (match FA conventions)
-               "Toy Play" = 'black',
+               "Toy Play"  = 'black',
                "Attention" = 'white',
-               "Demand" = 'white',
-               "Tangible" = 'black'
+               "Demand"    = 'white',
+               "Tangible"  = 'black'
              )) %>%
   scr_xlabel("Session") %>%                                 # Override x-axis label (bottom only shown by default)
   scr_ylabel("Combined Target Behavior (Per Minute)") %>%   # Override y-axis label (centered, leftmost label)
   scr_title("Analog Functional Analysis") %>%
-  scr_legend(position = "topright",                         # Specify legend
-             legend = c("Toy Play",                         # labels to include
+  scr_legend(position = "topright",                         # Specify legend location
+             legend = c("Toy Play",                         # labels to include (ordered)
                         "Attention",
                         "Demand",
                         "Tangible"),
-             col    = c('black',                            # color of markers
+             col    = c('black',                            # color of markers (ordered)
                         'white',
                         'white',
                         'black'),
-             lty      = c(1, 1, 1, 1),                      # line types
-             pch      = c(16, 22, 24, 8),                   # marker types
+             lty      = c(1, 1, 1, 1),                      # line types (ordered)
+             pch      = c(16, 22, 24, 8),                   # marker types (ordered)
              bty      = "n",                                # remove border
              pt.cex   = 2.25,                               # point size scale
              cex      = 1.25,                               # text size scale
