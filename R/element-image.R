0a6558ebe8d19cf088b6f43c27925a561c0439a9
Shawn Gilroy <shawnpgilroy@gmail.com>
2023-03-17 10:38:04 -0500

    Add in start of custom imgs

---
+++ R/element-image.R
@@ -0,0 +1,30 @@
+#' scr_images
+#'
+#' @param core_frame fxl object
+#' @param image should be RGML image
+#' @param cex from base
+#' @param mapping (optional) if overriding draw (i.e., different response)
+#'
+#' @author Shawn Gilroy <sgilroy1@@lsu.edu>
+#'
+#' @export
+scr_images <- function(core_frame,
+                       image,
+                       cex = 1,
+                       mapping) {
+
+  if (missing(image))
+    stop('must specify image to draw')
+
+  newlayer <- list()
+  newlayer[["type"]] <- "image"
+  newlayer[["image"]] <- image
+  newlayer[["cex"]] <- cex
+  newlayer[["aesthetics"]] <- NA
+
+  if (!missing(mapping))  newlayer[["aesthetics"]] <- rlang::enexpr(mapping)
+
+  core_frame$layers[[(length(core_frame[["layers"]]) + 1)]] <- newlayer
+
+  core_frame
+}
---
+++ demo/concurrentplot_fun.R
@@ -0,0 +1,220 @@
+library(fxl) # core plotting methods
+library(grImport)
+
+data <- Gilroyetal2021
+
+heart <- readPicture("heart-cairo-mod.ps.xml")
+star <- readPicture("star-cairo-mod.ps.xml")
+
+scr_plot(
+  data,
+  aesthetics = var_map(
+    x = Session,
+    y = Responding,
+    p = Condition,
+    facet = Participant
+  ),
+  mai = c(0.375,
+          0.375,
+          0.175,
+          0.1),
+  omi = c(0.25,
+          0.25,
+          0.25,
+          0.25)
+) |>
+scr_xoverride(
+  c(0.5, 25),
+  xticks = 1:25
+) |>
+scr_yoverride(
+  list(
+    "John" = list(
+      y0 = -1,
+      y1 = 20,
+      yticks = c(0, 5, 10, 15, 20)
+    ),
+    "Anthony" = list(
+      y0 = -0.5,
+      y1 = 10,
+      yticks = c(0, 5, 10)
+    ),
+    "Charles" = list(
+      y0 = -1,
+      y1 = 20,
+      yticks = c(0, 5, 10, 15, 20)
+    )
+  ),
+  ydelta = 5
+) |>
+scr_lines() |>
+scr_lines(
+  lty = 2,
+  mapping = list(
+    x = Session,
+    y = Reinforcers
+  )
+) |>
+scr_images(
+  image = heart,
+  cex = 0.03
+) |>
+scr_images(
+  image = star,
+  cex = 0.03,
+  mapping = list(
+    x = Session,
+    y = Reinforcers
+  )
+) |>
+scr_label_phase(
+  facet = "John",
+  cex = 1.25,
+  adj = 0.5,
+  y = 20,
+  labels = list(
+    "Baseline" = list(
+      x = 2
+    ),
+    "FR-Lowest" = list(
+      x = 5
+    ),
+    "Baseline" = list(
+      x = 8
+    ),
+    "FR-Inelastic" = list(
+      x = 11
+    ),
+    "FR-Elastic" = list(
+      x = 14
+    ),
+    "FR-Inelastic" = list(
+      x = 18
+    )
+  )
+) |>
+scr_label_facet(
+  cex = 1.5,
+  adj = 1,
+  x = 25,
+  labels = list(
+    "John" = list(
+      y = 2.5
+    ),
+    "Anthony" = list(
+      y = 12
+    ),
+    "Charles" = list(
+      y = 25
+    )
+  )) |>
+scr_plines_mbd(lines = list(
+  "A" = list(
+    "John" = list(
+      x1 = 3.5,
+      y1 = 20
+    ),
+    "Anthony" = list(
+      x1 = 3.5,
+      y1 = 10
+    ),
+    "Charles" = list(
+      x1 = 3.5,
+      y1 = 20,
+      y2 = -1
+    )
+  ),
+  "B" = list(
+    "John" = list(
+      x1 = 6.5,
+      y1 = 20
+    ),
+    "Anthony" = list(
+      x1 = 6.5,
+      y1 = 10
+    ),
+    "Charles" = list(
+      x1 = 8.5,
+      y1 = 20,
+      y2 = -1
+    )
+  ),
+  "C" = list(
+    "John" = list(
+      x1 = 9.5,
+      y1 = 20
+    ),
+    "Anthony" = list(
+      x1 = 9.5,
+      y1 = 10
+    ),
+    "Charles" = list(
+      x1 = 11.5,
+      y1 = 20,
+      y2 = -1
+    )
+  ),
+  "D" = list(
+    "John" = list(
+      x1 = 12.5,
+      y1 = 20
+    ),
+    "Anthony" = list(
+      x1 = 16.5,
+      y1 = 10
+    ),
+    "Charles" = list(
+      x1 = 16.5,
+      y1 = 20,
+      y2 = -1
+    )
+  ),
+  "E" = list(
+    "John" = list(
+      x1 = 15.5,
+      y1 = 20,
+      y2 = 2
+    ),
+    "Anthony" = list(
+      x1 = 22.5,
+      y1 = 10
+    ),
+    "Charles" = list(
+      x1 = 19.5,
+      y1 = 20,
+      y2 = -1
+    )
+  )
+)) |>
+scr_xlabel("Session") |>
+scr_ylabel("         Frequency (Responses, Reinforcers Delivered)") |>
+scr_title("Individual Evaluations of Reinforcer Efficacy and Elasticity across Reinforcers") |>
+scr_legend(
+  panel = "John",
+  position = "right",
+  legend = c("Responses Observed",
+             "Reinforcers Produced"),
+  col = c("black",
+          "black"),
+  pt_bg = c("black",
+            "black"),
+  lty = c(1,
+          2),
+  pch = c(19,
+          2),
+  bg = c("black",
+         "black"),
+  bty = "n",
+  pt_cex = 2.25,
+  cex = 1.25,
+  text_col = "black",
+  horiz = FALSE,
+  box_lty = 0
+)
+
+# |>
+#   scr_save(name = "../man/figures/concurrentfigure_fun.svg",
+#            format = "svg",
+#            units = "in",
+#            height = 6,
+#            width = 9)
