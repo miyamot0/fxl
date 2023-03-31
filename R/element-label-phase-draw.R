5cf78bca0807975f0220ddde4422be6afeaca5b9
shawn gilroy <shawnpgilroy@gmail.com>
2023-03-30 21:44:43 -0500

    Add in handling for blank facets

--- R/element-label-phase-draw.R
+++ R/element-label-phase-draw.R
@@ -10,9 +10,61 @@
 #'
 #' @export
 draw_label_phase <- function(core_frame, current_layer, facet_name) {
-  is_facet_name_na <- (is.na(facet_name))
+  if (!is.na(facet_name) && !is.na(current_layer$facet) && current_layer$facet == facet_name) {
+    for (lindex in seq_len(length(current_layer$labels))) {
+      label <- names(current_layer$labels)[lindex]
 
-  if (current_layer$facet == facet_name | is_facet_name_na) {
+      current_label <- current_layer$labels[[lindex]]
+
+      label <- ifelse(
+        is.null(label),
+        current_label, label
+      )
+
+      temp_x <- ifelse(
+        "x" %in% names(current_label),
+        current_label[["x"]], current_layer$x
+      )
+
+      temp_y <- ifelse(
+        "y" %in% names(current_label),
+        current_label[["y"]], current_layer$y
+      )
+
+      font_c <- ifelse(
+        "font" %in% names(current_label),
+        current_label[["font"]], 1
+      )
+
+      srt <- ifelse(
+        "srt" %in% names(current_label),
+        current_label[["srt"]], 0
+      )
+
+      temp_color <- ifelse(
+        "color" %in% names(current_label),
+        current_label[["color"]], "black"
+      )
+
+      temp_label <- ifelse(
+        "label" %in% names(current_label),
+        current_label[["label"]], label
+      )
+
+      text(
+        x      = temp_x,
+        y      = temp_y,
+        cex    = current_layer[["cex"]],
+        adj    = current_layer[["adj"]],
+        font   = current_layer[["face"]],
+        col    = temp_color,
+        srt    = srt,
+        labels = temp_label
+      )
+    }
+  }
+
+  if (is.na(facet_name)) {
     for (lindex in seq_len(length(current_layer$labels))) {
       label <- names(current_layer$labels)[lindex]
 
--- R/element-phasechange-draw.R
+++ R/element-phasechange-draw.R
@@ -51,4 +51,46 @@ draw_scr_plines <- function(core_frame, current_layer, facet_name) {
       )
     }
   }
+
+  if (is.na(facet_name)) {
+    for (key in names(current_layer$lines)) {
+      l_lty <- current_layer[["lty"]]
+      l_x1 <- current_layer$lines[[key]][["x1"]]
+      l_x2 <- ifelse(
+        is.null(current_layer$lines[[key]][["x2"]]),
+        l_x1, current_layer$lines[[key]][["x2"]]
+      )
+
+      l_y1 <- ifelse(
+        is.null(current_layer$lines[[key]][["y1"]]),
+        0, current_layer$lines[[key]][["y1"]]
+      )
+      l_y2 <- ifelse(
+        is.null(current_layer$lines[[key]][["y2"]]),
+        0, current_layer$lines[[key]][["y2"]]
+      )
+
+      temp_y1 <- ifelse(
+        l_y1 == 0, -((as.numeric(core_frame$dims[["max.local.y"]]) -
+                        as.numeric(core_frame$dims[["min.local.y"]])) *
+                       0.04), current_layer$lines[[key]][["y1"]]
+      )
+
+      temp_y2 <- ifelse(
+        l_y2 == 0, -((as.numeric(core_frame$dims[["max.local.y"]]) -
+                        as.numeric(core_frame$dims[["min.local.y"]])) *
+                       0.04), l_y2
+      )
+
+      if ("lty" %in% names(current_layer$lines[[key]])) {
+        l_lty <- current_layer$lines[[key]]$lty
+      }
+
+      lines(
+        c(l_x1, l_x2),
+        c(temp_y1, temp_y2),
+        lty = l_lty
+      )
+    }
+  }
 }
