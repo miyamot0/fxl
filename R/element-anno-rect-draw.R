fec9f456087ae59ee9e5bf3efa7f9486941617ad
shawn gilroy <shawnpgilroy@gmail.com>
2023-04-01 23:27:09 -0500

    Add in greyed out annotations

---
+++ R/element-anno-rect-draw.R
@@ -0,0 +1,63 @@
+#' draw_rect
+#'
+#' drawing function
+#'
+#' @param core_frame fxl object
+#' @param current_layer layer to be drawn
+#' @param facet_name name of facet
+#' @param zero_axis filter out all but zeros
+#'
+#' @author Shawn Gilroy <sgilroy1@@lsu.edu>
+#'
+#' @export
+draw_rect <- function(core_frame, current_layer, facet_name, zero_axis = FALSE) {
+  if (!is.na(current_layer$facet) & current_layer$facet == facet_name) {
+    for (rindex in seq_len(length(current_layer$rects))) {
+      current_rect <- current_layer$rects[[rindex]]
+
+      current_col <- current_layer$color
+
+      if ("color" %in% names(current_rect)) {
+        current_col <- current_rect[["color"]]
+      }
+
+      current_fill <- current_layer$fill
+
+      if ("fill" %in% names(current_rect)) {
+        current_fill <- current_rect[["fill"]]
+      }
+
+      rect(xleft   = as.numeric(current_rect[["x0"]]),
+           xright  = as.numeric(current_rect[["x1"]]),
+           ybottom = as.numeric(current_rect[["y0"]]),
+           ytop    = as.numeric(current_rect[["y1"]]),
+           col     = current_fill,
+           border  = current_col)
+    }
+  } else if (!is.na(facet_name)) {
+    for (rname in names(current_layer$rects)) {
+
+      if (rname == facet_name) {
+        current_rect <- current_layer$rects[[rname]]
+        current_col <- current_layer$color
+
+        if ("color" %in% names(current_rect)) {
+          current_col <- current_rect[["color"]]
+        }
+
+        current_fill <- current_layer$fill
+
+        if ("fill" %in% names(current_rect)) {
+          current_fill <- current_rect[["fill"]]
+        }
+
+        rect(xleft   = as.numeric(current_rect[["x0"]]),
+             xright  = as.numeric(current_rect[["x1"]]),
+             ybottom = as.numeric(current_rect[["y0"]]),
+             ytop    = as.numeric(current_rect[["y1"]]),
+             col     = current_fill,
+             border  = current_col)
+      }
+    }
+  }
+}
--- R/element-anno-rect.R
+++ R/element-anno-rect.R
@@ -1,37 +1,30 @@
 #' scr_rect
 #'
 #' @param core_frame fxl object
+#' @param rects list of keyed entries to be drawn on respective facets
+#' @param facet the facet which will be drawn upon
+#' @param color from base
+#' @param fill from base
 #' @param x1 from base
 #' @param x2 from base
 #' @param y1 from base
 #' @param y2 from base
-#' @param facet from base
-#' @param color from base
-#' @param fill from base
 #'
 #' @author Shawn Gilroy <sgilroy1@@lsu.edu>
 #'
 #' @export
 #' @importFrom rlang enexprs
 scr_rect <- function(core_frame,
-                     x1 = NA,
-                     x2 = NA,
-                     y1 = NA,
-                     y2 = NA,
+                     rects = NULL,
                      facet = NA,
                      color = "black",
                      fill = "black") {
   newlayer <- list()
   newlayer[["type"]] <- "rectangle"
+  newlayer[["rects"]] <- rects
   newlayer[["color"]] <- color
   newlayer[["fill"]] <- fill
   newlayer[["facet"]] <- facet
-  newlayer[["coords"]] <- list(
-    x1 = x1,
-    x2 = ifelse(is.na(x2), x1, x2),
-    y1 = y1,
-    y2 = ifelse(is.na(y2), y1, y2)
-  )
 
   core_frame$layers[[(length(core_frame[["layers"]]) + 1)]] <- newlayer
 
